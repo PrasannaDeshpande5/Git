0633b24cca5b182c53115a0bd44c5b5c9c1056a5
PrasannaDeshpande5 <prasanna.deshpande@rlard.com>
2017-09-27 16:27:04 +0530

    Third Commit

--- app/src/main/java/com/rlard/rlard008/git/MainActivity.java
+++ app/src/main/java/com/rlard/rlard008/git/MainActivity.java
@@ -2,12 +2,20 @@ package com.rlard.rlard008.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (TextView) findViewById(R.id.name);
+
+        name.setText("Valar Morgalis");
     }
 }
